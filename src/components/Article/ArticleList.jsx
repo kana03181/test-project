941da6b0345d1d298093244068a67623c26c7b02
itaikana <2015edcckanaitai@gmail.com>
2026-02-01 15:28:35 +0900

    課題3の実装 2回目の修正

--- src/components/Article/ArticleList.jsx
+++ src/components/Article/ArticleList.jsx
@@ -11,7 +11,6 @@ export default function ArticleList() {
     useEffect(() => {
     const fetcher = async () => {
       try {
-        setLoading(true);
         setError(null);
 
         const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts");
