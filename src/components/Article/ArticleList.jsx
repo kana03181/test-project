63a1ad1d5e80bec8c5e4a1fec257fd0f53e6da05
itaikana <2015edcckanaitai@gmail.com>
2026-02-01 14:59:36 +0900

    課題3の実装 修正1回目

--- src/components/Article/ArticleList.jsx
+++ src/components/Article/ArticleList.jsx
@@ -3,19 +3,62 @@ import { useState, useEffect } from "react";
 import ArticleItem from "./ArticleItem";
 import components from "./Article.module.css";
 
-export default function ArticleList({ src }) {
+export default function ArticleList() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-
-  useEffect(() => {
+    useEffect(() => {
     const fetcher = async () => {
-      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts");
-      const data = await res.json();
-      setPosts(data.posts);
-    }
+      try {
+        setLoading(true);
+        setError(null);
 
+        const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts");
+
+
+        if (!res.ok) {
+          throw new Error("記事が見つかりませんでした");
+        }
+
+        const data = await res.json();
+        setPosts(data.posts);
+
+      } catch (err) {
+        setError(err.message);
+
+      } finally {
+        setLoading(false);
+      }
+    };
     fetcher();
   }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <p>読み込み中...</p>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>エラー：{error}</p>
+      </div>
+    )
+  }
+
+  if (posts.length === 0) {
+    return (
+      <div>
+        <p>記事が見つかりません</p>
+      </div>
+    )
+  }
+
+
 
   return (
     <div className={components.article}>
